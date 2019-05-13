ae4a943aa4a94f495a4fd9d3d556887552016380
ZoranKJava <38503197+ZoranKJava@users.noreply.github.com>
2019-05-13 16:15:11 +0200

    Inheritance

---
+++ Vezbanje3/ImagePost.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vezbanje3
+{
+    //ImagePost derives from Post and adds a property (ImageURL) and two constructors
+    class ImagePost:Post
+    {
+        public string ImageURL { get; set; }
+
+        public ImagePost()
+        {
+
+        }
+
+        public ImagePost(string title, string sendByUsername, string imageURL, bool isPublic)
+        {
+            //The following properties and the GetNextID method are inherited from Post
+            this.ID = GetNextID();
+            this.Title = title;
+            this.SendByUsername = sendByUsername;
+            //Property ImageURL is a member of ImagePost, but not of Post
+            this.ImageURL = imageURL;
+            this.isPublic = isPublic;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"{this.ID} - {this.Title} - {this.ImageURL} - {this.SendByUsername} ");
+        }
+
+    }
+}
---
+++ Vezbanje3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vezbanje3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post1 = new Post("'-Thanks for the birthday wishes!!!'", true, "Zoran Kovacevic" );
+            Console.WriteLine(post1.ToString());
+
+            ImagePost imagePost1 = new ImagePost("Check out my new shoes", "Zoran Kovacevic", "https://images.com//shoes", true);
+            Console.WriteLine(imagePost1.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
