d05427b8c52e86531617d0194a6f305d49e1035d
ZoranKJava <38503197+ZoranKJava@users.noreply.github.com>
2019-05-13 17:33:37 +0200

    Video player - cmd

--- Vezbanje3/Program.cs
+++ Vezbanje3/Program.cs
@@ -10,13 +10,34 @@ namespace Vezbanje3
     {
         static void Main(string[] args)
         {
-            Post post1 = new Post("'-Thanks for the birthday wishes!!!'", true, "Zoran Kovacevic" );
+            Post post1 = new Post("'Thanks for the birthday wishes!!!'", true, "Zoran Kovacevic" );
             Console.WriteLine(post1.ToString());
 
-            ImagePost imagePost1 = new ImagePost("Check out my new shoes", "Zoran Kovacevic", "https://images.com//shoes", true);
+            ImagePost imagePost1 = new ImagePost("'Check out my new shoes'", "Zoran Kovacevic", "https://images.com//shoes", true);
             Console.WriteLine(imagePost1.ToString());
+
+            VideoPost videoPost1 = new VideoPost("'C# Video tutorial'", "Zoran Kovacevic", "http://fakevideo.com/video", true, 30);
+            Console.WriteLine(videoPost1.ToString());
+
+            videoPost1.Play();
+           
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\nPress any key to stop the video!\n");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("\nPlaying...");
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;                    
+            Console.ReadKey();
+            
+            videoPost1.Stop();
+
 
             Console.ReadKey();
         }
+       
     }
 }
---
+++ Vezbanje3/VideoPost.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Vezbanje3
+{
+    class VideoPost:Post
+    {
+        //Member fields
+        protected bool isPlaying = false;
+        protected int currDuration = 0;
+        Timer timer;
+
+
+        //Properties
+        protected string VideoURL { get; set; }
+        public int Length { get; set; }
+        public VideoPost() { }
+
+        public VideoPost(string title, string sendByUsername, string videoURL, bool isPublished, int length)
+        {
+            //Folowing properties and the GetNextID method are inherited from Post
+            this.ID = GetNextID();
+            this.Title = title;
+            this.SendByUsername = sendByUsername;
+            this.isPublic = isPublic;
+            //Property VideoURL is a member of VideoPost, but not of Post
+            this.VideoURL = videoURL;            
+            this.Length = length;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"{this.ID} - {this.Title} - {this.VideoURL} - {this.Length} - {this.SendByUsername} ");
+        }
+
+        public void Play()
+        {
+            if (!isPlaying)
+            {
+                isPlaying = true;
+                
+                timer = new Timer(TimerCallback, null, 0, 500);
+            }
+            
+        }
+
+        private void TimerCallback(Object o)
+        {
+            if(currDuration < Length)
+            {
+                currDuration++;
+                Console.Write($"  ");
+                GC.Collect();
+            }
+            else
+            {
+                Stop();
+            }
+        }
+
+        public void Stop()
+        {
+            if (isPlaying)
+            {
+                isPlaying = false;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine($"Stopped at {currDuration}");
+                currDuration = 0;
+                timer.Dispose();
+            }
+            
+        }
+
+    }
+}
