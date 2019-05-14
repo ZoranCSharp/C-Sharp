b776b7c960c668e457cdc9437a00ec70d1073312
ZoranKJava <38503197+ZoranKJava@users.noreply.github.com>
2019-05-14 09:49:22 +0200

    Inheritance employee

---
+++ Vezbanje3/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vezbanje3
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public int Salary { get; set; }
+
+        public Employee()
+        {
+            Name = "Kovacevic";
+            FirstName = "Denis";
+            Salary = 50000;
+
+        }
+        public Employee(string name, string firstName, int salary)
+        {
+            this.Name = name;
+            this.FirstName = firstName;
+            this.Salary = salary;
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("\nI'm working");
+        }
+
+        public void Pause()
+        {
+            Console.WriteLine("\nI'm having a break!");
+        }
+    }
+}
--- Vezbanje3/Program.cs
+++ Vezbanje3/Program.cs
@@ -37,6 +37,24 @@ namespace Vezbanje3
 
 
             Console.ReadKey();
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Employee michael = new Employee("Michael", "Miller", 40000);
+
+            michael.Work();
+            michael.Pause();
+           
+
+            Boss chuckNorris = new Boss("Ferrari", "Norris", "Chuck", 999999999);
+
+            chuckNorris.Lead();
+
+            Trainee michelle = new Trainee(32, 8, "Gartner", "Michelle", 10000);
+            michelle.Learn();
+            michelle.Work();
+            Console.ReadLine();
+
         }
        
     }
