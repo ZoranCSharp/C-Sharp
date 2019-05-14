b9d4886ae50ca57a369230677e3f254fe66316ba
ZoranKJava <38503197+ZoranKJava@users.noreply.github.com>
2019-05-14 14:19:05 +0200

    Lambda, delegates

---
+++ AdvancedCsharp/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedCsharp
+{
+    class Number
+    {
+        public int n { get; set; }
+    }
+}
