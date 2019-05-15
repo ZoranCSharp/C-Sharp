6f5893c39812988082a03073e89b96f3c87aa427
ZoranKJava <38503197+ZoranKJava@users.noreply.github.com>
2019-05-15 10:38:43 +0200

    DateTime, substract

--- RegularExpressions/Program.cs
+++ RegularExpressions/Program.cs
@@ -30,8 +30,73 @@ namespace RegularExpressions
                 GroupCollection group = hit.Groups;
                 Console.WriteLine($"{group[0].Value} found at {group[0].Index}");
             }
+            DateTime dateTime = new DateTime(2018, 5, 31);
 
+            Console.WriteLine($"My birthday is {dateTime}");
+
+            //write today on screen
+            Console.WriteLine(DateTime.Today);
+
+            //Write current time on sccreen
+            Console.WriteLine(DateTime.Now);
+            
+            DateTime tomorrow = GetTomorrow();
+            Console.WriteLine($"Tomorrow will be the {tomorrow}");
+
+            Console.WriteLine($"Today is: {DateTime.Today.DayOfWeek}");
+
+            Console.WriteLine($"First day of a year: {GetFirstDayOfYear(1999)}");
+
+            int days = DateTime.DaysInMonth(2000, 2);
+            Console.WriteLine($"Days in Feb 2000: {days}");
+            days = DateTime.DaysInMonth(2001, 2);
+            Console.WriteLine($"Days in Feb 2001: {days}");
+            days = DateTime.DaysInMonth(2004, 2);
+            Console.WriteLine($"Days in Feb 2004: {days}");
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine($"Minute: {now.Minute}");
+
+            Console.WriteLine($"{now.Hour} o'clock {now.Minute} minutes and {now.Second}");
+
+            Console.WriteLine("Write date in this format: yyyy-mm-dd");
+            string input = Console.ReadLine();
+            if(DateTime.TryParse(input, out dateTime))
+            {
+                Console.WriteLine(dateTime);
+                TimeSpan daysPassed = now.Subtract(dateTime);
+                Console.WriteLine($"Days passed since: {daysPassed.Days}");
+            }
+            else
+            {
+                Console.WriteLine("Wrong input!");
+            }
+
+            Console.WriteLine("Write birth date in this format: yyyy-mm-dd");
+
+            string input2 = Console.ReadLine();
+
+            if(DateTime.TryParse(input2,  out dateTime))
+            {
+                Console.WriteLine(dateTime);
+                TimeSpan daysPassed = now.Subtract(dateTime);
+                Console.WriteLine($"You are {daysPassed.Days} days old!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong input!!!");
+            }
             Console.ReadLine();
+        }
+
+        static DateTime GetTomorrow()
+        {
+            return DateTime.Today.AddDays(1);
+        }
+
+        static DateTime GetFirstDayOfYear(int year)
+        {
+            return new DateTime(year, 1, 1);
         }
     }
 }
