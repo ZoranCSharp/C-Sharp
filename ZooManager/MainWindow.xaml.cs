047ea76b262cbe0ed6caf90c7c53fd23f1f1a4ff
ZoranKJava <38503197+ZoranKJava@users.noreply.github.com>
2019-05-17 12:40:24 +0200

    ListBox Data

--- ZooManager/MainWindow.xaml.cs
+++ ZooManager/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,11 +23,46 @@ namespace ZooManager
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        SqlConnection sqlConnection;
         public MainWindow()
         {
+            
             InitializeComponent();
 
-            string connectionString = ConfigurationManager.ConnectionStrings["WPF_ZooManager.Properties.Settings.ZooConnectionString"].ConnectionString;
+            //Connection to MS SQL
+            string connectionString = ConfigurationManager.ConnectionStrings["ZooManager.Properties.Settings.ZooConnectionString"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString);
+
+            ShowZoos();
+        }
+
+        private void ShowZoos()
+        {
+            try
+            {
+                string query = "select * from Zoo";
+                //the SqlAdapter can be imagined like an Interface to make Tables usable by C#-Objects
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (sqlDataAdapter)
+                {
+                    DataTable zooTable = new DataTable();
+                    sqlDataAdapter.Fill(zooTable);
+
+                    //Which Information of the Table in DataTable should be shown in our ListBox
+                    listZoos.DisplayMemberPath = "Location";
+                    //Which Value should be delivered, when an Item from our ListBox is selected
+                    listZoos.SelectedValuePath = "Id";
+                    //The Reference to the Data the ListBox should populate
+                    listZoos.ItemsSource = zooTable.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            
         }
     }
 }
