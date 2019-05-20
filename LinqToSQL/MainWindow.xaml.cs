a9587797e64223bcd89dc10893022927556753c5
ZoranKJava <38503197+ZoranKJava@users.noreply.github.com>
2019-05-20 16:09:45 +0200

    inserting objects in database

--- LinqToSQL/MainWindow.xaml.cs
+++ LinqToSQL/MainWindow.xaml.cs
@@ -30,6 +30,49 @@ namespace LinqToSQL
             //import system.configuration from references
             string connectionstring = ConfigurationManager.ConnectionStrings["LinqToSQL.Properties.Settings.ZooConnectionString"].ConnectionString;
             dataContex = new LinqToSqlDataClassesDataContext(connectionstring);
+
+            //InsertUniversities();
+
+            InsertStudents();
+        }
+
+
+
+        public void InsertUniversities()
+        {
+            dataContex.ExecuteCommand("delete from University");
+
+            University yale = new University();
+            yale.Name = "Yale";            
+            dataContex.Universities.InsertOnSubmit(yale);
+
+            University bjgTech = new University();
+            bjgTech.Name = "Beijing Tech";
+            dataContex.Universities.InsertOnSubmit(bjgTech);
+
+            dataContex.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContex.Universities;
+        }
+
+        public void InsertStudents()
+        {
+            //"from university in dataContent.University" + 
+            //    "where university == "Yale" select university"
+            University yale = dataContex.Universities.First(un => un.Name.Equals("Yale"));
+            University beijingTech = dataContex.Universities.First(un => un.Name.Equals("Beijing Tech"));
+
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student { Name = "Carla", Gender = "female", UniversityId = yale.Id });
+            students.Add(new Student { Name = "Toni", Gender = "male", University = yale });
+            students.Add(new Student { Name = "Leyle", Gender = "female", UniversityId = beijingTech.Id });
+            students.Add(new Student { Name = "Jame", Gender = "trans-gender", University = beijingTech });
+
+            dataContex.Students.InsertAllOnSubmit(students);
+            dataContex.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContex.Students;
         }
     }
 }
