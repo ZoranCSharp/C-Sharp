1aa22cce7d874968bbbd7d8c6ac5c0949819a8d6
ZoranKJava <38503197+ZoranKJava@users.noreply.github.com>
2019-05-20 15:15:37 +0200

    Linq to SQL

---
+++ LinqToSQL/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].StudentLecture
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [StudentId] INT NOT NULL, 
+    [LectureId] INT NOT NULL, 
+    CONSTRAINT [StudentFK] FOREIGN KEY ([StudentId]) REFERENCES [dbo].[Student]([Id]) ON DELETE CASCADE,
+	CONSTRAINT [LectureFK]  FOREIGN KEY ([LectureId]) REFERENCES [dbo].[Lecture]([Id]) ON DELETE CASCADE
+)
