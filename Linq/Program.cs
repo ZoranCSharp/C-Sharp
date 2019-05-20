9366e76247af1976640bfb52838bc554e65e502f
ZoranKJava <38503197+ZoranKJava@users.noreply.github.com>
2019-05-20 09:39:29 +0200

    Linq

---
+++ Linq/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            OddNumbers(number);
+            Console.ReadKey();
+        }
+
+        static void OddNumbers(int[] numbers)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Odd Number: ");
+
+            IEnumerable<int> oddNumbers = from number in numbers where number % 2 != 0 select number;
+
+            Console.WriteLine(oddNumbers+"\n");
+            
+            foreach(int i in oddNumbers)
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
