71954343ef731a0328ef2eb2e0c69e940d0e0538
ZoranKJava <38503197+ZoranKJava@users.noreply.github.com>
2019-05-21 10:57:46 +0200

    ThreadPools and Threads in Background

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -12,20 +12,45 @@ namespace Threads
         static void Main(string[] args)
         {
 
-            var taskCompletionSource = new TaskCompletionSource<bool>();
-            var thread = new Thread(() =>
-            {
-                Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started!");
-                Thread.Sleep(1000);
-                taskCompletionSource.TrySetResult(true);
-                Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} ended!");
-            });
+            //var taskCompletionSource = new TaskCompletionSource<bool>();
+            //var thread = new Thread(() =>
+            //{
+            //    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started!");
+            //    Thread.Sleep(5000);
+            //    taskCompletionSource.TrySetResult(true);
+            //    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} ended!");
+            //});
 
+
+            //thread.Start();
+            //var test = taskCompletionSource.Task.Result;
+            //Console.WriteLine($"Task was done {test}");
+
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 4");
+            })
+            { IsBackground = true }.Start();
+
+
+            Enumerable.Range(0, 100).ToList().ForEach(f =>
+             {
+                 ThreadPool.QueueUserWorkItem((o =>
+                 {
+                     new Thread(() =>
+                     {
+                         Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started!");
+                         Thread.Sleep(5000);
+
+                         Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} ended!");
+                     }).Start();
+                 }));
+                 
+             });
            
-            thread.Start();
-            var test = taskCompletionSource.Task.Result;
-            Console.WriteLine($"Task was done {test}");
-           
+
 
 
             Console.ReadLine();
