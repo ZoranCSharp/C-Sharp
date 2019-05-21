40cace0ceafa1bf3df465446e027b4a9b79635e9
ZoranKJava <38503197+ZoranKJava@users.noreply.github.com>
2019-05-21 10:17:38 +0200

    Threads

---
+++ Threads/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Hello World!");
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Hello World!");
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Hello World!");
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Hello World!");
+            //Thread.Sleep(1000);
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread One");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread Two");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread Three");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread Four");
+            }).Start();
+
+           
+            Console.ReadLine();
+        }
+    }
+}
